6fbf22689568694b7e3aa7bd418c88dc5346233f
dezsirazvan <dezsirazvan@gmail.com>
2024-04-11 15:34:31 +0200

    fix

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,34 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-
+import * as path from 'path';
+import * as fs from 'fs';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "rspec-file-generator" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('rspec-file-generator.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Rspec File Generator!');
+	let disposable = vscode.commands.registerCommand('extension.generateRSpec', (uri: vscode.Uri) => {
+		const filePath = uri.fsPath;
+		const fileName = path.basename(filePath);
+		const dirName = path.dirname(filePath);
+	
+		// Assuming the code files are in 'app' and specs should be in 'spec'
+		const specDir = dirName.replace('/app/', '/spec/');
+		const specFileName = fileName.replace('.rb', '_spec.rb');
+		const specFilePath = path.join(specDir, specFileName);
+	
+		// Create spec directory if it doesn't exist
+		if (!fs.existsSync(specDir)) {
+			fs.mkdirSync(specDir, { recursive: true });
+		}
+	
+		// Create the spec file if it doesn't exist
+		if (!fs.existsSync(specFilePath)) {
+			fs.writeFileSync(specFilePath, `require 'rails_helper'\n\ndescribe ${fileName.replace('.rb', '')} do\nend`);
+			vscode.window.showInformationMessage(`RSpec file created: ${specFilePath}`);
+		} else {
+			vscode.window.showInformationMessage('RSpec file already exists.');
+		}
 	});
 
 	context.subscriptions.push(disposable);
