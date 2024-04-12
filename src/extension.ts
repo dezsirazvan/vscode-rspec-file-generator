ef5f486af920d4c27e8a701b1e57f5cdf025a911
dezsirazvan <dezsirazvan@gmail.com>
2024-04-12 09:36:59 +0200

    get correct model name

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,7 @@ import * as path from 'path';
 import * as fs from 'fs';
 
 export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand('extension.generateRSpec', (uri?: vscode.Uri) => {
+    let disposable = vscode.commands.registerCommand('extension.generateRSpec', async (uri?: vscode.Uri) => {
         if (!uri) {
             let activeEditor = vscode.window.activeTextEditor;
             if (activeEditor) {
@@ -26,7 +26,11 @@ export function activate(context: vscode.ExtensionContext) {
         }
 
         if (!fs.existsSync(specFilePath)) {
-            fs.writeFileSync(specFilePath, `require 'rails_helper'\n\ndescribe ${fileName.replace('.rb', '')} do\nend`);
+            const fileContents = fs.readFileSync(filePath, 'utf8');
+            const fullClassName = getFullClassName(fileContents);
+            const describeBlock = fullClassName ? fullClassName : fileName.replace('.rb', '');
+
+            fs.writeFileSync(specFilePath, `require 'rails_helper'\n\ndescribe '${describeBlock}' do\nend`);
             vscode.window.showInformationMessage(`RSpec file created: ${specFilePath}`);
         } else {
             vscode.window.showInformationMessage('RSpec file already exists.');
@@ -34,4 +38,17 @@ export function activate(context: vscode.ExtensionContext) {
     });
 
     context.subscriptions.push(disposable);
+}
+
+function getFullClassName(fileContents: string) {
+    const classMatcher = /module\s+(\w+)|class\s+([\w:]+)/g;
+    let match;
+    let fullClassName = '';
+
+    while ((match = classMatcher.exec(fileContents)) !== null) {
+        const part = match[1] || match[2];
+        fullClassName += (fullClassName ? '::' : '') + part;
+    }
+
+    return fullClassName;
 }
